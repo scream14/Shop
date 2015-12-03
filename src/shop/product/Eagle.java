e935ba22c7b0144e1180ffd0417b4c944a20c294
scream14 <mogilserg14@gmail.com>
2015-12-03 18:00:57 +0200

    Create Eagle.java

---
+++ src/shop/product/Eagle.java
@@ -0,0 +1,12 @@
+package shop.product;
+
+
+public class Eagle extends ABird{
+
+    public Eagle(int count, double price){
+        name = "Eagle";
+        this.price = price;
+        this.count += count;
+    }
+
+}
